c985833529d3a7874bb3a8da510ec1baf377e177
LangKhon69 <lekhangls103@gmail.com>
2025-12-08 10:38:40 +0700

    Test trên Desktop

--- CodingSQL.php
+++ CodingSQL.php
@@ -2,3 +2,4 @@
 //Đây là bản tin thời sự ngày 8/12/20205 : Hôm nay đơn giản sẽ có 3 nhiệm vụ chính
 //Chưa bao giờ tôi cảm thấy tập trung bào công việc như bây giờ
 //Hãy chú ý lại cách fetch
+//Hãy thử lại 1 lần nữa và tìm ra cách hiểu hơn
