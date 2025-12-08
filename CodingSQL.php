2495952a26b7c80b9ee48a2f2fb2a3fc66b776b4
LangKhon69 <lekhangls103@gmail.com>
2025-12-08 10:22:43 +0700

    Thực hành trên Desktop

--- CodingSQL.php
+++ CodingSQL.php
@@ -1,3 +1,4 @@
 //Hello các bạn 
 //Đây là bản tin thời sự ngày 8/12/20205 : Hôm nay đơn giản sẽ có 3 nhiệm vụ chính
-// Chưa bao giờ tôi cảm thấy tập trung bào công việc như bây giờ
+//Chưa bao giờ tôi cảm thấy tập trung bào công việc như bây giờ
+//Hãy chú ý lại cách fetch
